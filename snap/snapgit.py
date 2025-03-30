67e533b0cc8a77621a126c891d32bcba32e999d3
Sackey Ezekiel Etrue <sackeyetrue@gmail.com>
2025-03-30 00:52:25 +0000

    package for simplicity

---
+++ snap/snapgit.py
@@ -0,0 +1,72 @@
+"""Snap"""
+
+import os
+from snap.init import SnapInit
+from snap.branch import Branch
+
+
+class SnapGit:
+    """SnapGit"""
+
+    def __init__(self):
+        self.branch: str = self.get_branch()
+        self.branches: list[Branch] = self.load_branches()
+
+    def init(self, description):
+        """add"""
+        snapinit = SnapInit()
+        snapinit.init(description)
+        self.branches.append(Branch("main"))
+        self.checkout("main")
+
+    def new_branch(self, name):
+        """add"""
+        self.branches.append(Branch(name))
+
+    def get_branch(self):
+        """add"""
+        branch = ""
+        try:
+            with open(os.path.join(".snap", "head"), "r", encoding="utf-8") as f:
+                branch = f.read()
+        except OSError:
+            pass
+        return branch
+
+    def load_branches(self):
+        """add"""
+        branches: list[Branch] = []
+
+        for _, _, files in os.walk(os.path.join(".", ".snap", "branch"), topdown=True):
+            for file in files:
+                branches.append(Branch(file))
+
+        return branches
+
+    def checkout(self, branch):
+        """add"""
+        try:
+            with open(os.path.join(".snap", "head"), "w", encoding="utf-8") as f:
+                f.write(branch)
+        except OSError:
+            pass
+
+    def add(self, files):
+        """add"""
+        for branch in self.branches:
+            if branch.name == self.branch:
+                branch.add_index(files)
+
+    def commit(self, message=""):
+        """add"""
+        for branch in self.branches:
+            if branch.name == self.branch:
+                branch.commit_branch(message)
+
+    def revert(self, hash_digest):
+        """add"""
+        for branch in self.branches:
+            if branch.name == self.branch:
+                branch.revert_branch(hash_digest)
+
+
